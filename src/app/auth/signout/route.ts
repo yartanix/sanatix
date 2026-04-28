5f28a3d79c0b6cfb82dea51d8c3b1ce0b57d23ad
yartanix <yartanix@gmail.com>
2026-04-28 11:46:48 +0300

    feat: phase2

---
+++ src/app/auth/signout/route.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+export async function POST(req) {
+  const supabase = await createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+  if (user) await supabase.auth.signOut();
+  revalidatePath("/", "layout");
+  return NextResponse.redirect(new URL("/", req.url), { status: 302 });
+}
